23fdd174bd62f6249180d955e9594fb7ec5cbea3
wtucker <wtbtucker@gmail.com>
2022-09-13 16:14:05 -0400

    Add functions for add, subtract, multiply, and divide

--- jscript.js
+++ jscript.js
@@ -6,17 +6,54 @@ clear_button.addEventListener('click', function() {
 });
 
 const digits = document.querySelectorAll('.digit');
-digits.forEach(button => button.addEventListener('click', function(e) {
+digits.forEach(button => button.addEventListener('click', () => {
     display_text.textContent += e.target.innerText;
 }));
 
-// add
-// event listener to add button
+// display the plus?
+// wait for user to type more numbers
+    // would need to remember first operand and the plus operator
+
 const plus_button = document.getElementById('plus');
 plus_button.addEventListener('click', () => {
     operand_one = Number(display_text.textContent);
     display_text.textContent = '';
     return operand_one;
 })
-// when clicked store the textcontent of display in a variable
+
+
+function add(a,b) {
+    return Number(a) + Number(b);
+}
+
+function subtract(a,b) {
+    return Number(a) - Number(b);
+}
+
+function multiply(a,b) {
+    return Number(a) * Number(b);
+}
+
+function divide(a,b) {
+    return Number(a) / Number(b);
+}
+
+function operate(a, b, operator) {
+    let result = 0;
+    switch(true) {
+        case (operator === '+'):
+            result = add(a,b)
+            return result;
+        case (operator === '-'):
+            result = subtract(a,b)
+            return result;
+        case (operator === '*'):
+            result = multiply(a,b)
+            return result;
+        case (operator === '/'):
+            result = divide(a,b)
+            return result;
+    }
+}
 // wait for user to type more numbers
+
