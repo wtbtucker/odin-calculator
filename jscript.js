03c3fe9ce829840d342cd76f34a50f529ff8920a
wtucker <wtbtucker@gmail.com>
2022-09-13 14:19:58 -0400

    Append button text to display text

--- jscript.js
+++ jscript.js
@@ -1,6 +1,5 @@
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => button.addEventListener('click', function(e) {
-    console.log(e.target.innerText);
-    // append event target to display innertext
-    //
+    let display_text = document.querySelector('.display');
+    display_text.textContent += e.target.innerText;
 }));
