79352da328f8ce5f6f260b995609bc33be8e2203
wtucker <wtbtucker@gmail.com>
2022-09-13 15:58:40 -0400

    Add event listener to +, store first operand in variable

--- jscript.js
+++ jscript.js
@@ -9,3 +9,14 @@ const digits = document.querySelectorAll('.digit');
 digits.forEach(button => button.addEventListener('click', function(e) {
     display_text.textContent += e.target.innerText;
 }));
+
+// add
+// event listener to add button
+const plus_button = document.getElementById('plus');
+plus_button.addEventListener('click', () => {
+    operand_one = Number(display_text.textContent);
+    display_text.textContent = '';
+    return operand_one;
+})
+// when clicked store the textcontent of display in a variable
+// wait for user to type more numbers
